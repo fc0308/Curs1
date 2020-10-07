304aafe58de58bff0ffa684c1d6cd2cc1e337777
contributor <62571926+fc0308@users.noreply.github.com>
2020-10-07 23:14:47 +0300

    Add project files.

---
+++ Style.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Curs1
+{
+    /// <summary>
+    /// Interaction logic for Style.xaml
+    /// </summary>
+    public partial class Style : Window
+    {
+        public Style()
+        {
+            InitializeComponent();
+        }
+    }
+}
